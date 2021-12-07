70c8843e047189d319a024c705af747e66c2837e
Nitish <57482317+Nkr001@users.noreply.github.com>
2021-12-07 19:41:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+export default function CalendarGfg() {
+const [value, onChange] = useState(new Date());
+
+return (
+	<div>
+	<h1>Calendar - GeeksforGeeks</h1>
+	<Calendar
+		onChange={onChange}
+		value={value}
+	/>
+	</div>
+);
 }
 
-
-export default App;
